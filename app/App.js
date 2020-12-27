3af76930ee8cd8dd2a0ede3665a13f67516c3421
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-12-27 16:34:46 +0100

    basic background status and nav bars ui

--- app/App.js
+++ app/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import {SafeAreaView, Text, StatusBar} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  StatusBar,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+
+//! pick one of thise backgrounds
+import bg1 from './assets/bg1.jpg';
+import bg2 from './assets/bg2.jpg';
+import bg3 from './assets/bg3.jpg';
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>Wind Converter</Text>
-      </SafeAreaView>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+
+      <ImageBackground source={bg3} style={styles.imagebackground}>
+        <SafeAreaView style={styles.container}>
+          <Text>Wind Converter</Text>
+          <Text style={styles.copyright}>© 2020 Jakub Olejnik</Text>
+        </SafeAreaView>
+      </ImageBackground>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  imagebackground: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  container: {
+    paddingTop: StatusBar.currentHeight,
+    height: '100%',
+    backgroundColor: 'rgba(1, 1, 1, 0.8)',
+  },
+  copyright: {
+    position: 'absolute',
+    bottom: 2,
+    left: 2,
+    color: 'gray',
+    fontStyle: 'italic',
+    fontSize: 11,
+  },
+});
 
 export default App;
